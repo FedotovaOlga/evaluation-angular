126600e44f596c14b84196f023ea03f5b574fedb
Olga Fedotova <olgaalex1994@gmail.com>
2025-11-15 11:50:05 +0100

    routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FormationsComponent } from './components/formations/formations';
+import { HomeComponent } from './components/home/home';
+import { AuthComponent } from './components/auth/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'formations', component: FormationsComponent },
+  { path: 'auth', component: AuthComponent },
+];
