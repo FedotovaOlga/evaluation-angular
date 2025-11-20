728544dcda214bc0797484dc019e26430fc82c4c
Olga Fedotova <olgaalex1994@gmail.com>
2025-11-20 16:51:55 +0100

    guard on /formations added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { FormationsComponent } from './components/formations/formations';
 import { HomeComponent } from './components/home/home';
 import { AuthComponent } from './components/auth/auth';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'formations', component: FormationsComponent },
+  { path: 'formations', component: FormationsComponent, canActivate: [authGuard] },
   { path: 'auth', component: AuthComponent },
 ];
